a2e269ca5f9fce71086c467891aa703a5a5ed3de
paidaxin <2353144379@qq.com>
2025-06-25 14:23:03 +0800

    YiMo诞生

--- src/main/java/top/paidaxin/YiMoApplication.java
+++ src/main/java/top/paidaxin/YiMoApplication.java
@@ -2,8 +2,10 @@ package top.paidaxin;
 
 import org.h2.tools.Server;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -13,10 +15,13 @@ import java.util.HashSet;
 @MapperScan("top.paidaxin.dao")
 public class YiMoApplication {
     public static void main(String[] args) throws SQLException {
+        /*
+            启动H2数据库服务
+         */
         Server server = Server.createTcpServer(
-                "-tcp",              // 启动 TCP 模式
-                "-tcpAllowOthers",  // 允许外部连接（默认只允许本机）
-                "-tcpPort", "9092"  // 指定端口
+                "-tcp",
+                "-tcpAllowOthers",
+                "-tcpPort", "9092"
         ).start();
 
         SpringApplication.run(YiMoApplication.class, args);
