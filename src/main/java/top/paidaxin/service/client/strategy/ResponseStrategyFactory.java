0f44838ded4597100c6c5e4cb858ee5d0b9a4393
paidaxin <2353144379@qq.com>
2025-09-19 10:16:01 +0800

    新增： YiMo后端支持stream返回

---
+++ src/main/java/top/paidaxin/service/client/strategy/ResponseStrategyFactory.java
@@ -0,0 +1,27 @@
+package top.paidaxin.service.client.strategy;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author paidaxin
+ * @date 2025-09-19:09:54
+ */
+@Service
+public class ResponseStrategyFactory {
+    @Resource
+    private Map<String, IResponseStrategy> responseStrategyMap;
+
+    /**
+     * 根据不同的返回类型处理返回
+     */
+    public Object handleResponse(HttpServletResponse response, Integer statusCode, String contentType, String data, Long delay) {
+        IResponseStrategy strategyMapOrDefault = responseStrategyMap.getOrDefault(contentType, responseStrategyMap.get(MediaType.APPLICATION_JSON_VALUE));
+        return strategyMapOrDefault.writeResponse(data, delay);
+    }
+
+}
---
+++ src/main/java/top/paidaxin/service/client/strategy/StreamResponseStrategy.java
@@ -0,0 +1,37 @@
+package top.paidaxin.service.client.strategy;
+
+import org.junit.jupiter.api.Order;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Order(2)
+@Service(MediaType.TEXT_EVENT_STREAM_VALUE)
+public class StreamResponseStrategy implements IResponseStrategy {
+
+    /**
+     * stream流返回根据回车进行切分
+     *
+     * @param data  接口返回值
+     * @param delay 睡眠时间
+     */
+    @Override
+    public Object writeResponse(String data, Long delay) {
+        //1.异常判断
+        if (!StringUtils.hasText(data)) return null;
+
+        //2.换行切割data
+        List<String> streamData = Arrays.stream(data.split("\n"))
+                .filter(StringUtils::hasText)
+                .collect(Collectors.toList());
+
+        //3.非阻塞返回
+        return Flux.fromIterable(streamData).delayElements(Duration.ofMillis(delay));
+    }
+}
